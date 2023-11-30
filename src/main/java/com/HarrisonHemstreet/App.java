ffc6e35331c55f0bd52d8fb120e501aed36a26a2
Harry <harrisonhemstreet@proton.me>
2023-11-30 12:23:38 -0700

    first commit

---
+++ src/main/java/com/HarrisonHemstreet/App.java
@@ -0,0 +1,18 @@
+package com.HarrisonHemstreet;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+  public static void main(String[] args) {
+    // primitive type: int
+    // int age = 30;
+
+    // reference type: Date
+    Date now = new Date();
+    System.out.println(now);
+  }
+}
