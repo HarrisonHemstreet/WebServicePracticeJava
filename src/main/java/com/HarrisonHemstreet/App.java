75a9a0f6420794d87b9e55ed9faf505d7202184e
Harry <harrisonhemstreet@proton.me>
2023-11-30 13:04:43 -0700

    clean up code

--- src/main/java/com/HarrisonHemstreet/App.java
+++ src/main/java/com/HarrisonHemstreet/App.java
@@ -1,17 +1,12 @@
 package com.HarrisonHemstreet;
 
-import java.awt.Point;
-
 /**
  * Hello world!
  *
  */
 public class App {
   public static void main(String[] args) {
-    Point point1 = new Point(1, 1);
-    Point point2 = point1;
-    point2.x = 2;
-    System.out.println("point1:" + point1);
-    System.out.println("point2:" + point2);
+    String message = "hi";
+    System.out.println(message + " dave");
   }
 }
