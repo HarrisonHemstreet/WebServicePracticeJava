76bd7bdc0e41f44cf851f26c9645c8aa23d7a6f1
Harry <harrisonhemstreet@proton.me>
2023-11-30 13:02:44 -0700

    learned pass by ref

--- src/main/java/com/HarrisonHemstreet/App.java
+++ src/main/java/com/HarrisonHemstreet/App.java
@@ -1,6 +1,6 @@
 package com.HarrisonHemstreet;
 
-import java.util.Date;
+import java.awt.Point;
 
 /**
  * Hello world!
@@ -8,11 +8,10 @@ import java.util.Date;
  */
 public class App {
   public static void main(String[] args) {
-    // primitive type: int
-    // int age = 30;
-
-    // reference type: Date
-    Date now = new Date();
-    System.out.println(now);
+    Point point1 = new Point(1, 1);
+    Point point2 = point1;
+    point2.x = 2;
+    System.out.println("point1:" + point1);
+    System.out.println("point2:" + point2);
   }
 }
